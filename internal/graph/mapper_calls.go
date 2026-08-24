22140107d422dac2485824bba076ea65c1fa20be
SamuelMolero36 <samueljosemolero63@gmail.com>
2026-08-24 09:51:12 -0500

    fix: cleaning based on comments

--- internal/graph/mapper_calls.go
+++ internal/graph/mapper_calls.go
@@ -60,9 +60,8 @@ func collectMapperCalls(files []mapperFile) ([]mapperFileCalls, mapperStats) {
 			stats.parseErr++
 			continue
 		}
-
-		refs := callsFromMapperFile(eng, f, src, &stats)
-		if len(refs) > 0 {
+		
+		if refs := callsFromMapperFile(eng, f, src, &stats);  len(refs) > 0 {
 			out = append(out, mapperFileCalls{file: f.rel, lang: f.entry.Name, refs: refs})
 		}
 	}
